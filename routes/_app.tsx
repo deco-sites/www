5f1590135c9cbd3b59eb2ae6d023c6c87bfc57aa
gimenes <tlgimenes@gmail.com>
2023-01-24 18:40:40 -0300

    add tracking script

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,16 +1,14 @@
 import { AppProps } from "$fresh/server.ts";
 import { context } from "$live/live.ts";
-import GoogleTagManager from "partytown/integrations/GoogleTagManager.tsx";
+import ScriptGTM from "partytown/integrations/GTM.tsx";
 
-const trackingId = "";
+const trackingId = "GTM-P6D23BB";
 
 export default function App(props: AppProps) {
   return (
     <>
       {/* Add Tag Manager script during production only. To test it locally remove the condition */}
-      {!!context.deploymentId && trackingId && (
-        <GoogleTagManager trackingId={trackingId} />
-      )}
+      {!!context.deploymentId && <ScriptGTM trackingId={trackingId} />}
       <props.Component />
     </>
   );

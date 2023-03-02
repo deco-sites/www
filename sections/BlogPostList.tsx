ab990038046bb3a289636f357f27289f4663ba82
Fernando <fernando.lima@ccc.ufcg.edu.br>
2023-03-02 16:31:06 -0300

    Added image component

--- sections/BlogPostList.tsx
+++ sections/BlogPostList.tsx
@@ -1,5 +1,6 @@
 import { LoaderReturnType } from "$live/std/types.ts";
 import { getBlogPath, PostList } from "$deco/components/utils/Blog.ts";
+import Image from "$live/std/ui/components/Image.tsx";
 
 export type Props = {
   postList: LoaderReturnType<string>;
@@ -26,32 +27,34 @@ export default function MarkdownContainer(props: Props) {
             top: 0,
           }}
         />
-        <section class="max-w-4xl sm:p-0 p-8 pt-10 sm:pt-20">
+        <section class="max-w-4xl lg:px-0  px-8 pt-10 sm:pt-20">
           <div class="w-full">
             <a
               href={getBlogPath(highlightPost.path)}
               class="group w-full"
             >
               <div class="text-deco-dark-green mb-10 sm:mb-20">
-                <div class="">
-                  <img
-                    height="502"
-                    width="894"
-                    src={highlightPost.img}
-                    class="w-full mb-6 h-full"
-                    
-                  />
-                </div>
-
-                <h1 class="font-bold text-5xl leading-10 sm:leading-[62px] text-default group-hover:underline">
-                  {highlightPost.title}
-                </h1>
-                <div class="text-2xl leading-7 my-3">
-                  {highlightPost.descr}
-                </div>
-                <div>
-                  <span class="mr-5">{highlightPost.date}</span>
-                  <span>{highlightPost.author}</span>
+                <Image
+                  src={highlightPost.img}
+                  sizes="(max-width: 894px) 100vw, 50vw"
+                  width={894}
+                  height={502}
+                  loading="eager"
+                  decoding="async"
+                  fetchPriority="high"
+                  preload
+                />
+                <div class="mt-6">
+                  <h1 class="font-bold text-5xl leading-10 sm:leading-[62px] text-default group-hover:underline">
+                    {highlightPost.title}
+                  </h1>
+                  <div class="text-2xl leading-7 my-3">
+                    {highlightPost.descr}
+                  </div>
+                  <div>
+                    <span class="mr-5">{highlightPost.date}</span>
+                    <span>{highlightPost.author}</span>
+                  </div>
                 </div>
               </div>
             </a>
@@ -59,25 +62,29 @@ export default function MarkdownContainer(props: Props) {
 
           <div class="grid grid-cols-1 md:grid-cols-2 md:gap-8">
             {normalPosts.map((post) => (
-              <a href={getBlogPath(post.path)} class="group">
+              <a href={getBlogPath(post.path)} class="group mx-auto">
                 <div class="w-full text-deco-dark-green mb-10 sm:mb-20">
-                  <img
-                    height="240px"
-                    width="427px"
+                  <Image
                     src={post.img}
-                    class="w-full mb-5 h-full"
-                    style="width: 427px; height: 340px; object-fit: cover;"
+                    sizes="(max-width: 432px) 100vw, 50vw"
+                    width={432}
+                    height={344}
+                    loading="eager"
+                    decoding="async"
+                    fetchPriority="high"
+                    preload
                   />
-
-                  <h1 class="font-bold text-5xl leading-9 text-default group-hover:underline">
-                    {post.title}
-                  </h1>
-                  <div class="text-2xl leading-7 my-3">
-                    {post.descr}
-                  </div>
-                  <div>
-                    <span class="mr-5">{post.date}</span>
-                    <span>{post.author}</span>
+                  <div class="mt-6">
+                    <h1 class="font-bold text-5xl leading-9 text-default group-hover:underline">
+                      {post.title}
+                    </h1>
+                    <div class="text-2xl leading-7 my-3">
+                      {post.descr}
+                    </div>
+                    <div>
+                      <span class="mr-5">{post.date}</span>
+                      <span>{post.author}</span>
+                    </div>
                   </div>
                 </div>
               </a>
